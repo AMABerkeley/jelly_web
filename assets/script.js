ca0bd7228c30be0c706d62bc7f7f5d1b4249bb65
Joshua Yuan <joshua43210@gmail.com>
2019-05-02 17:11:57 -0700

    implement roslibjs kind of, and add new gitignore

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+var command = "roll_stop";
 $(document).ready(function() {
     $("#rolling_button").on("click", function() {
         console.log("clicked rolling button");
@@ -22,4 +23,39 @@ $(document).ready(function() {
         }
     });
 
+
+    var ros = new ROSLIB.Ros();
+
+    // If there is an error on the backend, an 'error' emit will be emitted.
+    ros.on('error', function(error) {
+        console.log(error);
+    });
+    // Find out exactly when we made a connection.
+    ros.on('connection', function() {
+        console.log('Connection made!');
+    });
+    ros.on('close', function() {
+        console.log('Connection closed.');
+    });
+    // Create a connection to the rosbridge WebSocket server.
+    ros.connect('ws://localhost:9090');
+
+    var control = new ROSLIB.Topic({
+        ros : ros,
+        name : '/jelly',
+        messageType : 'std_msgs/String'
+    });
+
+
+    $(".control i").on("click", function() {
+        command = $(this).attr("id");
+        console.log(command);
+        var message = new ROSLIB.Message({
+            data : command
+        });
+        control.publish(message);
+    });
+
 });
+
+
