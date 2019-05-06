6ecade0dcba9cb0bb9e9e9ece93ff69be9725c53
Joshua Yuan <joshua43210@gmail.com>
2019-05-06 03:37:53 -0700

    add state response and fast rolling

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,33 @@
-var command = "roll_stop";
+var command = "stand_stop";
+var command_to_sentence = {
+    "stand_stop": "I am standing still.",
+    "stand_left": "I am twisting to the left.",
+    "stand_right": "I am twisting to the right",
+    "roll_forward": "I am rolling forward.",
+    "roll_forward_fast": "I am rolling forward faster.",
+    "roll_back": "I am rolling backwards.",
+    "roll_back_fast": "I am rolling backwards faster.",
+    "roll_stop": "I am not rolling now.",
+    "walk_forward": "I am walking forward.",
+    "walk_back": "I am walking backwards.",
+    "walk_left": "I am walking to the left.",
+    "walk_right": "I am walking to the right.",
+    "walk_stop": "I am not walking now.",
+    "walking_mode": "Walking Mode.",
+    "standing_mode": "Standing Mode.",
+    "rolling_mode": "Rolling Mode",
+    "rolling_button": "roll_stop",
+    "standing_button": "stand_stop",
+    "walking_button": "walk_stop"
+};
+var ros = new ROSLIB.Ros();
+var control = new ROSLIB.Topic({
+    ros : ros,
+    name : '/jelly_gui/command',
+    messageType : 'std_msgs/String'
+});
+
+
 $(document).ready(function() {
     $("#rolling_button").on("click", function() {
         console.log("clicked rolling button");
@@ -7,9 +36,12 @@ $(document).ready(function() {
             $("#rolling_panel").show();
             $("#standing_panel").hide();
             $("#walking_panel").hide();
+            $("#log p").text(command_to_sentence["rolling_mode"]);
             $(this).addClass("button-primary");
             $("#standing_button").removeClass("button-primary");
             $("#walking_button").removeClass("button-primary");
+
+            command = command_to_sentence[$(this).attr("id")];
         }
     });
     $("#standing_button").on("click", function() {
@@ -19,9 +51,12 @@ $(document).ready(function() {
             $("#rolling_panel").hide();
             $("#standing_panel").show();
             $("#walking_panel").hide();
+            $("#log p").text(command_to_sentence["standing_mode"]);
             $("#rolling_button").removeClass("button-primary");
             $(this).addClass("button-primary");
             $("#walking_button").removeClass("button-primary");
+
+            command = command_to_sentence[$(this).attr("id")];
         }
     });
 
@@ -33,14 +68,18 @@ $(document).ready(function() {
             $("#rolling_panel").hide();
             $("#standing_panel").hide();
             $("#walking_panel").show();
+            $("#log p").text(command_to_sentence["walking_mode"]);
             $("#rolling_button").removeClass("button-primary");
             $("#standing_button").removeClass("button-primary");
             $(this).addClass("button-primary");
+
+            command = command_to_sentence[$(this).attr("id")];
         }
     });
 
+    $(".control i").hover();
 
-    var ros = new ROSLIB.Ros();
+
 
     // If there is an error on the backend, an 'error' emit will be emitted.
     ros.on('error', function(error) {
@@ -58,11 +97,6 @@ $(document).ready(function() {
     ros.connect('ws://localhost:9090');
 
     setInterval(function() {
-        var control = new ROSLIB.Topic({
-            ros : ros,
-            name : '/jelly_gui/command',
-            messageType : 'std_msgs/String'
-        });
         var message = new ROSLIB.Message({
             data : command
         });
@@ -70,6 +104,7 @@ $(document).ready(function() {
         $(".control i").on("click", function() {
             command = $(this).attr("id");
             console.log(command);
+            $("#log p").text(command_to_sentence[command]);
         });
         control.publish(message);
     }, 100);
